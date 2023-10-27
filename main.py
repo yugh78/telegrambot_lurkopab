418d5824976d33846a068c881d8a63e6a1ef4afb
evgeiniy <zhenya_timofeev_99_99@mail.ru>
2023-10-27 18:50:59 +0300

    закончена работа с апи вк

--- main.py
+++ main.py
@@ -2,14 +2,33 @@ import asyncio
 from database import DatabaseContext
 from models.post import Post
 from models.picture import Picture
-import vk
+from vk import VkClient,sort_collection
+
 import sys
 import json
+def fill_db():
+    vk_client = VkClient(sys.argv[2])
+    post_count = vk_client.get_post_count()
+    posts = []
+    offset = 0
+    while post_count > 0:
+        posts = sort_collection(vk_client.get_data(offset))
+        for post in posts:
+            print(post['text'])
+        offset += 100
+        post_count -= 100
 
 
 async def main_async():
-    r = vk.get_data2(sys.argv[1])
-    print(json.dumps(r, indent=4))
+    if sys.argv[1] == 'fill-database':
+        fill_db()
+
+    elif sys.argv[1] == 'startbot':
+        pass
+    else: print('Wrong command')
+
+
+    #print(json.dumps(r, indent=4))
     #s = {'items':r}
     #print(json.dumps(r))
     #print(json.dumps(s,indent=2))
--- vk.py
+++ vk.py
@@ -3,22 +3,33 @@ import json
 import vk_api
 
 
-def get_data2(app_id: str):
-    session = vk_api.VkApi(token=app_id)
-    vk = session.get_api()
-    posts = vk.wall.get(v=5.154, domain='lurkopub_alive', count=10, filter='all')
-    return posts['items']
+class VkClient:
+    def __init__(self, app_id):
+        self.app_id = app_id
+
+    def get_data(self, offset: int):
+        session = vk_api.VkApi(token=self.app_id)
+        vk = session.get_api()
+        posts = vk.wall.get(offset=offset, v=5.154, domain='lurkopub_alive', count=100, filter='all')
+        # log('get response ...', posts['items'])
+        return posts['items']
+
+    def get_post_count(self):
+        session = vk_api.VkApi(token=self.app_id)
+        vk = session.get_api()
+        posts = vk.wall.get(v=5.154, domain='lurkopub_alive', count=0, filter='all')
+        return posts['count']
 
 
-def get_data(app_id: str):
-    url = 'https://api.vk.com/method/wall.get'
-    r = requests.post(url,
-                      params={'v': 5.154, 'domain': 'lurkopub_alive', 'count': 10, 'filter': 'all'},
-                      headers={'Authorization': f"Bearer {app_id}"})
-    res = json.loads(r.text)
-
-    # fl = filter(lambda item: item['inner_type'] == 'wall_wallpost', res['response']['items'])
-    return res['response']['items']
+# def get_data(app_id: str):
+#     url = 'https://api.vk.com/method/wall.get'
+#     r = requests.post(url,
+#                       params={'v': 5.154, 'domain': 'lurkopub_alive', 'count': 10, 'filter': 'all'},
+#                       headers={'Authorization': f"Bearer {app_id}"})
+#     res = json.loads(r.text)
+#
+#     # fl = filter(lambda item: item['inner_type'] == 'wall_wallpost', res['response']['items'])
+#     return res['response']['items']
 
 
 def sort_collection(coll):
@@ -26,8 +37,22 @@ def sort_collection(coll):
     for x in coll:
         if is_right_item(x):
             result.append(x)
+            log('item is right:', x)
+
     return result
 
 
 def is_right_item(item):
-    return item['inner_type'] == 'wall_wallpost' and '#паста' in item['description']
+    return item['inner_type'] == 'wall_wallpost' \
+        and '#паста' in item['text']
+
+
+enable_log = True
+
+
+def log(msg, *args):
+    global enable_log
+    if enable_log:
+        if msg:
+            print(f"\033[32m{msg}\033[0;0m")
+        print(json.dumps(args, indent=2))
