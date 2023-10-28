a71d605142e8a2f3f895890f9e1e9a1cd02eda5d
Katy248 <petrovanton247@gmail.com>
2023-10-28 10:31:03 +0300

    Simple refactor

--- vk.py
+++ vk.py
@@ -2,6 +2,7 @@ import requests
 import json
 import vk_api
 
+WALL_DOMAIN = 'lurkopub_alive'
 
 class VkClient:
     def __init__(self, app_id):
@@ -10,27 +11,15 @@ class VkClient:
     def get_data(self, offset: int):
         session = vk_api.VkApi(token=self.app_id)
         vk = session.get_api()
-        posts = vk.wall.get(offset=offset, v=5.154, domain='lurkopub_alive', count=100, filter='all')
+        posts = vk.wall.get(offset=offset, v=5.154, domain=WALL_DOMAIN, count=100, filter='all')
         # log('get response ...', posts['items'])
         return posts['items']
 
     def get_post_count(self):
         session = vk_api.VkApi(token=self.app_id)
         vk = session.get_api()
-        posts = vk.wall.get(v=5.154, domain='lurkopub_alive', count=0, filter='all')
+        posts = vk.wall.get(v=5.154, domain=WALL_DOMAIN, count=0, filter='all')
         return posts['count']
-
-
-# def get_data(app_id: str):
-#     url = 'https://api.vk.com/method/wall.get'
-#     r = requests.post(url,
-#                       params={'v': 5.154, 'domain': 'lurkopub_alive', 'count': 10, 'filter': 'all'},
-#                       headers={'Authorization': f"Bearer {app_id}"})
-#     res = json.loads(r.text)
-#
-#     # fl = filter(lambda item: item['inner_type'] == 'wall_wallpost', res['response']['items'])
-#     return res['response']['items']
-
 
 def sort_collection(coll):
     result = []
